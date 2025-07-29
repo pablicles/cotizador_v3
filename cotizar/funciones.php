b674d5f7f38e5f946609af4a7b6cf924d7d66871
Pablo <pablomiranda@millop.com>
2025-07-28 21:05:43 -0600

    Funcion de datos cajas actualizada

--- cotizar/funciones.php
+++ cotizar/funciones.php
@@ -65,258 +65,194 @@ function get_cajas_proximas(mysqli $conn, float $l, float $w, float $h, int $lim
  * Para agregar un nuevo armado solo es necesario añadir una nueva entrada
  * en el arreglo armados dentro de la función.
  */
-function obtener_datos_caja(int $armado, float $largo, float $ancho, float $alto): array
-{
-    $armados = [
-        1 => function($L, $W, $H) {
-            $l = (($L + $W) * 2) + 7;
-            $a = $H + min($L, $W) + 3;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => 0,
-            ]];
-        },
-        2 => function($L, $W, $H) {
-            $l = ($L * 2) + ($W * 3);
-            $a = $H + ($W * 2);
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 6) + ($a * 7),
-            ]];
-        },
-        3 => function($L, $W, $H) {
-            $l = $L + ($H * 4) + 12;
-            $a = $W + ($H * 2) + 4;
-            $c = ($l * 4) + ($a * 10);
-            return [
-                [
-                    'largo_lamina' => $l,
-                    'ancho_lamina' => $a,
-                    'cm_suaje' => $c,
-                    'nombre' => 'Base',
-                ],
-                [
-                    'largo_lamina' => $l,
-                    'ancho_lamina' => $a,
-                    'cm_suaje' => $c,
-                    'nombre' => 'Tapa',
-                ],
-            ];
-        },
-        4 => function($L, $W, $H) {
-            $l = $L + ($H * 2) + 4;
-            $a = $W + ($H * 2) + 4;
-            $c = ($l * 4) + ($a * 5);
-            return [
-                [
-                    'largo_lamina' => $l,
-                    'ancho_lamina' => $a,
-                    'cm_suaje' => $c,
-                    'nombre' => 'Base',
-                ],
-                [
-                    'largo_lamina' => $l,
-                    'ancho_lamina' => $a,
-                    'cm_suaje' => $c,
-                    'nombre' => 'Tapa',
-                ],
-            ];
-        },
-        5 => function($L, $W, $H) {
-            $l = (($L + $W) * 2) + 7;
-            $a = $H + min($L, $W) + 3;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 4) + ($a * 7),
-            ]];
-        },
-        6 => function($L, $W, $H) {
-            $l1 = (($L + $W) * 2) + 7;
-            $a1 = $H + (min($L, $W) / 2) + 3;
-            $l2 = $L + 8;
-            $a2 = $W + 8;
-            return [
-                [
-                    'largo_lamina' => $l1,
-                    'ancho_lamina' => $a1,
-                    'cm_suaje' => 0,
-                    'nombre' => 'Base',
-                ],
-                [
-                    'largo_lamina' => $l2,
-                    'ancho_lamina' => $a2,
-                    'cm_suaje' => 0,
-                    'nombre' => 'Tapa',
-                ],
-            ];
-        },
-        7 => function($L, $W, $H) {
-            $l1 = (($L + $W) * 2) + 7;
-            $a1 = $H + (min($L, $W) / 2) + 3;
-            $c1 = ($l1 * 3) + ($a1 * 7);
-            $l2 = $L + 8;
-            $a2 = $W + 8;
-            $c2 = ($l2 * 4) + ($a2 * 4);
-            return [
-                [
-                    'largo_lamina' => $l1,
-                    'ancho_lamina' => $a1,
-                    'cm_suaje' => $c1,
-                    'nombre' => 'Base',
-                ],
-                [
-                    'largo_lamina' => $l2,
-                    'ancho_lamina' => $a2,
-                    'cm_suaje' => $c2,
-                    'nombre' => 'Tapa',
-                ],
-            ];
-        },
-        8 => function($L, $W, $H) {
-            $l = $L + ($H * 2) + 4;
-            $a = ($H * 3) + ($W * 2) + 4;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 7) + ($a * 6),
-            ]];
-        },
-        9 => function($L, $W, $H) {
-            $l = $L + ($H * 4) + 8;
-            $a = ($W * 2) + ($H * 3) + 5;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 5) + ($a * 5),
-            ]];
-        },
-        10 => function($L, $W, $H) {
-            $l = ($W * 2) + ($H * 4) + 4;
-            $a = $L + ($H * 2) + 4;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 5) + ($a * 5),
-            ]];
-        },
-        11 => function($L, $W, $H) {
-            $l = ($L * 2) + ($W * 2) + 6;
-            $a = $H + ($W * 2) + 4;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 4) + ($a * 5),
-            ]];
-        },
-        12 => function($L, $W, $H) {
-            $l = ($L * 2) + ($W * 2) + 6;
-            $a = $H + ($W * 2) + 5;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 4) + ($a * 5),
-            ]];
-        },
-        13 => function($L, $W, $H) {
-            $l = $L + ($H * 4) + 8;
-            $a = ($W * 2) + ($H * 3) + 4;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 7) + ($a * 6),
-            ]];
-        },
-        14 => function($L, $W, $H) {
-            $l = $L + ($H * 4) + 12;
-            $a = $W + ($H * 2) + 4;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 4) + ($a * 10),
-            ]];
-        },
-        15 => function($L, $W, $H) {
-            $l = $L + ($H * 2) + 4;
-            $a = $W + ($H * 2) + 4;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 4) + ($a * 5),
-            ]];
-        },
-        16 => function($L, $W, $H) {
-            $l = $L + ($H * 2) + 4;
-            $a = $W + ($H * 2) + 5;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 4) + ($a * 4),
-            ]];
-        },
-        17 => function($L, $W, $H) {
-            $l = $L + ($H * 2) + 4;
-            $a = $W + ($H * 2) + 5;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 4) + ($a * 5),
-            ]];
-        },
-        18 => function($L, $W, $H) {
-            return [[
-                'largo_lamina' => $L,
-                'ancho_lamina' => $W,
-                'cm_suaje' => 0,
-            ]];
-        },
-        19 => function($L, $W, $H) {
-            $l = (($L + $W) * 4) + 5;
-            $a = ($H + $W) + 3;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 3) + ($a * 6),
-            ]];
-        },
-        20 => function($L, $W, $H) {
-            $l = ($L * 2) + ($W * 2) + 6;
-            $a = ($W * 2) + $H;
-            return [[
-                'largo_lamina' => $l,
-                'ancho_lamina' => $a,
-                'cm_suaje' => ($l * 5) + ($a * 8) + (12 * 8),
-            ]];
-        },
-        21 => function($L, $W, $H) {
-            return [[
-                'largo_lamina' => $L,
-                'ancho_lamina' => $W,
-                'cm_suaje' => 0,
-            ]];
-        },
-        22 => function($L, $W, $H) {
-            return [[
-                'largo_lamina' => $L,
-                'ancho_lamina' => $W,
-                'cm_suaje' => 0,
-            ]];
-        },
-        23 => function($L, $W, $H) {
-            return [[
-                'largo_lamina' => $L,
-                'ancho_lamina' => $W,
-                'cm_suaje' => 0,
-            ]];
-        },
-    ];
+//Funcion que obtiene el tamaño de la lamina y costo del suaje de una caja
+function obtener_datos_caja($armado, $largo_caja, $ancho_caja, $alto_caja){
+    switch ($armado) {
+        case 1: //Estandar sin suaje
+            $datos_caja['largo_lamina'][0]=(($largo_caja+$ancho_caja)*2)+7;
+            $datos_caja['ancho_lamina'][0]=($alto_caja+min($largo_caja, $ancho_caja))+3;
+            $datos_caja['cm_suaje'][0]=0;
+            return $datos_caja;
+            break;
 
-    if (isset($armados[$armado])) {
-        return $armados[$armado]($largo, $ancho, $alto);
+        case 2: //Boxlunch      
+            $datos_caja['largo_lamina'][0]=($largo_caja*2)+($ancho_caja*3);
+            $datos_caja['ancho_lamina'][0]=($alto_caja)+($ancho_caja*2);
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*6)+($datos_caja['ancho_lamina'][0]*7);
+            return $datos_caja;
+            break;
+
+        case 3: //Con tapa reforzada
+            $datos_caja['largo_lamina'][0]=($largo_caja)+($alto_caja*4)+12;
+            $datos_caja['ancho_lamina'][0]=($ancho_caja)+($alto_caja*2)+4;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*4)+($datos_caja['ancho_lamina'][0]*10);
+            $datos_caja['nombre'][0] ="Base";
+            $datos_caja['largo_lamina'][1]=($largo_caja)+($alto_caja*4)+12;
+            $datos_caja['ancho_lamina'][1]=($ancho_caja)+($alto_caja*2)+4;
+            $datos_caja['cm_suaje'][1]=($datos_caja['largo_lamina'][1]*4)+($datos_caja['ancho_lamina'][1]*10);
+            $datos_caja['nombre'][1] ="Tapa ";
+            return $datos_caja;
+            break;
+
+        case 4: //Con tapa tipo rosca
+            $datos_caja['largo_lamina'][0]=($largo_caja)+($alto_caja*2)+4;
+            $datos_caja['ancho_lamina'][0]=($ancho_caja)+($alto_caja*2)+4;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*4)+($datos_caja['ancho_lamina'][0]*5);
+            $datos_caja['nombre'][0] ="Base";
+            $datos_caja['largo_lamina'][1]=($largo_caja)+($alto_caja*2)+4;
+            $datos_caja['ancho_lamina'][1]=($ancho_caja)+($alto_caja*2)+4;
+            $datos_caja['cm_suaje'][1]=($datos_caja['largo_lamina'][1]*4)+($datos_caja['ancho_lamina'][1]*5);
+            $datos_caja['nombre'][1] ="Tapa ";
+            return $datos_caja;
+            break;
+
+        case 5: //Caja estandar suajada
+            $datos_caja['largo_lamina'][0]=(($largo_caja+$ancho_caja)*2)+7;
+            $datos_caja['ancho_lamina'][0]=($alto_caja+min($largo_caja, $ancho_caja))+3;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*4)+($datos_caja['ancho_lamina'][0]*7);
+            return $datos_caja;
+            break;
+
+        case 6: //Estandar con tapa sin suaje
+            $datos_caja['largo_lamina'][0]=(($largo_caja+$ancho_caja)*2)+7; //Caja
+            $datos_caja['ancho_lamina'][0]=($alto_caja+(min($largo_caja, $ancho_caja))/2)+3;
+            $datos_caja['cm_suaje'][0]=0;
+            $datos_caja['nombre'][0] ="Base";
+            $datos_caja['largo_lamina'][1]=$largo_caja+8; //Tapa
+            $datos_caja['ancho_lamina'][1]=$ancho_caja+8;
+            $datos_caja['cm_suaje'][1]=0;
+            $datos_caja['nombre'][1] ="Tapa";
+            return $datos_caja;
+            break;
+
+        case 7: //Estandar con tapa suajada
+            $datos_caja['largo_lamina'][0]=(($largo_caja+$ancho_caja)*2)+7; //Caja
+            $datos_caja['ancho_lamina'][0]=($alto_caja+(min($largo_caja, $ancho_caja))/2)+3;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*3)+($datos_caja['ancho_lamina'][0]*7);
+            $datos_caja['nombre'][0] ="Base";
+            $datos_caja['largo_lamina'][1]=$largo_caja+8; //Tapa
+            $datos_caja['ancho_lamina'][1]=$ancho_caja+8;
+            $datos_caja['cm_suaje'][1]=($datos_caja['largo_lamina'][1]*4)+($datos_caja['ancho_lamina'][1]*4);
+            $datos_caja['nombre'][1] ="Tapa ";
+            return $datos_caja;
+            break;
+
+        case 8: //Tipo dona
+            $datos_caja['largo_lamina'][0]=($largo_caja)+($alto_caja*2)+4;
+            $datos_caja['ancho_lamina'][0]=($alto_caja*3)+($ancho_caja*2)+4;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*7)+($datos_caja['ancho_lamina'][0]*6);
+            return $datos_caja;
+            break;
+
+        case 9: //Tipo mailbox
+            $datos_caja['largo_lamina'][0]=($largo_caja)+($alto_caja*4)+8;
+            $datos_caja['ancho_lamina'][0]=($ancho_caja*2)+($alto_caja*3)+5;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*5)+($datos_caja['ancho_lamina'][0]*5);
+            return $datos_caja;
+            break;
+
+        case 10: //Tipo pizza
+            $datos_caja['largo_lamina'][0]=($ancho_caja*2)+($alto_caja*4)+4;
+            $datos_caja['ancho_lamina'][0]=$largo_caja+($alto_caja*2)+4;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*5)+($datos_caja['ancho_lamina'][0]*5);
+            return $datos_caja;
+            break;
+
+        case 11: //Taza autoarmable
+            $datos_caja['largo_lamina'][0]=($largo_caja*2)+($ancho_caja*2)+6;
+            $datos_caja['ancho_lamina'][0]=($alto_caja)+($ancho_caja*2)+4;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*4)+($datos_caja['ancho_lamina'][0]*5);
+            return $datos_caja;
+            break;
+
+        case 12: //Taza pegada
+            $datos_caja['largo_lamina'][0]=($largo_caja*2)+($ancho_caja*2)+6;
+            $datos_caja['ancho_lamina'][0]=$alto_caja+($ancho_caja*2)+5;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*4)+($datos_caja['ancho_lamina'][0]*5);
+            return $datos_caja;
+            break;
+
+        case 13: //Tipo zapatos
+            $datos_caja['largo_lamina'][0]=$largo_caja+($alto_caja*4)+8;
+            $datos_caja['ancho_lamina'][0]=($ancho_caja*2)+($alto_caja*3)+4;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*7)+($datos_caja['ancho_lamina'][0]*6);
+            return $datos_caja;
+            break;
+
+        case 14: //Charola reforzada
+            $datos_caja['largo_lamina'][0]=($largo_caja)+($alto_caja*4)+12;
+            $datos_caja['ancho_lamina'][0]=($ancho_caja)+($alto_caja*2)+4;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*4)+($datos_caja['ancho_lamina'][0]*10);
+            return $datos_caja;
+            break;
+
+        case 15: //Charola tipo rosca
+            $datos_caja['largo_lamina'][0]=($largo_caja)+($alto_caja*2)+4;
+            $datos_caja['ancho_lamina'][0]=($ancho_caja)+($alto_caja*2)+4;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*4)+($datos_caja['ancho_lamina'][0]*5);
+            return $datos_caja;
+            break;
+
+        case 16: //Inserto sencillo
+            $datos_caja['largo_lamina'][0]=$largo_caja+($alto_caja*2)+4;
+            $datos_caja['ancho_lamina'][0]=$ancho_caja+($alto_caja*2)+5;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*4)+($datos_caja['ancho_lamina'][0]*4);
+            return $datos_caja;
+            break;
+
+        case 17: //Inserto con pestañas
+            $datos_caja['largo_lamina'][0]=$largo_caja+($alto_caja*2)+4;
+            $datos_caja['ancho_lamina'][0]=$ancho_caja+($alto_caja*2)+5;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*4)+($datos_caja['ancho_lamina'][0]*5);
+            return $datos_caja;
+            break;
+
+        case 18: //Lámina
+            $datos_caja['largo_lamina'][0]=$largo_caja;
+            $datos_caja['ancho_lamina'][0]=$ancho_caja;
+            $datos_caja['cm_suaje'][0]=0;
+            return $datos_caja;
+            break;
+
+        case 19: //Palomitas
+            $datos_caja['largo_lamina'][0]=(($largo_caja+$ancho_caja)*4)+5;
+            $datos_caja['ancho_lamina'][0]=($alto_caja+$ancho_caja)+3;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*3)+($datos_caja['ancho_lamina'][0]*6);
+            return $datos_caja;
+            break;
+
+        case 20: //Bolsas
+            $datos_caja['largo_lamina'][0]=($largo_caja*2)+($ancho_caja*2)+6;
+            $datos_caja['ancho_lamina'][0]=($ancho_caja*2)+$alto_caja;
+            $datos_caja['cm_suaje'][0]=($datos_caja['largo_lamina'][0]*5)+($datos_caja['ancho_lamina'][0]*8)+(12*8);
+            return $datos_caja;
+            break;
+
+        case 21: //Lámina
+            $datos_caja['largo_lamina'][0]=$largo_caja;
+            $datos_caja['ancho_lamina'][0]=$ancho_caja;
+            $datos_caja['cm_suaje'][0]=0;
+            return $datos_caja;
+            break;
+
+        case 22: //Lámina
+            $datos_caja['largo_lamina'][0]=$largo_caja;
+            $datos_caja['ancho_lamina'][0]=$ancho_caja;
+            $datos_caja['cm_suaje'][0]=0;
+            return $datos_caja;
+            break;
+
+        case 23: //Lámina
+            $datos_caja['largo_lamina'][0]=$largo_caja;
+            $datos_caja['ancho_lamina'][0]=$ancho_caja;
+            $datos_caja['cm_suaje'][0]=0;
+            return $datos_caja;
+            break;
+        
+        default:
+            // code...
+            return $datos_caja;
+            break;
     }
-
-    return [];
 }
 ?>
